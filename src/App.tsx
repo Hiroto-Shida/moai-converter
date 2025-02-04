bd80ac39f6af5b28d4d9e9d88e929b718cf7bc14
Hiroto-Shida <uecshida6295@gmail.com>
2025-02-04 14:13:41 +0900

    :bug: 不要なmeta記述を削除

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,7 @@ import Main from "./components/Main";
 function App() {
   return (
     <>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* TODO: OGPの設定 */}
-        <link href="/dist/styles.css" rel="stylesheet" />
-        <title>モアイ翻訳</title>
-      </head>
-      <body>
-        <Main />
-      </body>
+      <Main />
     </>
   );
 }
