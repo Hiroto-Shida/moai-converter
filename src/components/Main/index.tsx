34cce52fc627c665919a13c8f6ddecd790ae7a76
Hiroto-Shida <uecshida6295@gmail.com>
2025-02-04 14:22:52 +0900

    :bug: ビルドエラーをなおす

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,7 +4,7 @@ import moaiLogo from "../../assets/moai.png";
 const Main: FC = () => {
   return (
     <div>
-      <h1 clasName="text-center text-2xl">Hello, Moai!</h1>
+      <h1 className="text-center text-2xl">Hello, Moai!</h1>
       <p>test</p>
       <img src={moaiLogo} className="w-[100px]" alt="moai logo" />
     </div>
