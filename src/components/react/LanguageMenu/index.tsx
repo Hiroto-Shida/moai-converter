02b20178e4044abd5275015212863c08ffca38df
Hiroto-Shida <uecshida6295@gmail.com>
2025-02-24 10:40:32 +0900

    :sparkles: 言語選択header,title,footerの追加

---
+++ src/components/react/LanguageMenu/index.tsx
@@ -0,0 +1,16 @@
+import GlobalIcon from "@assets/global.svg";
+
+const LanguageMenu: React.FC = () => {
+  return (
+    <div className="w-full">
+      {/* TODO: 言語切り替え機能を実装 */}
+      <img
+        src={GlobalIcon.src}
+        alt=""
+        className="m-0 ml-auto size-[20px] cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default LanguageMenu;
